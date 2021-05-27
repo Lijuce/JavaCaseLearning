c059ba37e1a8cc3489f2972570c27f131fab6d11
Lijuce <441214473@qq.com>
2021-05-27 20:04:38 +0800

    优化数据库工具类

--- TmallCase/src/main/java/utlis/DBUtil.java
+++ TmallCase/src/main/java/utlis/DBUtil.java
@@ -1,8 +1,6 @@
 package utlis;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBUtil {
     static String ip = "127.0.0.1";
@@ -11,22 +9,64 @@ public class DBUtil {
     static String encoding = "UTF-8";
     static String loginName = "root";
     static String password = "mysql";
+    static String driver = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(driver);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws SQLException {
-//        String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
-        String url = "jdbc:mysql://localhost:3306/tmall";
-        return DriverManager.getConnection(url, "root", "mysql");
+        String url = String.format("jdbc:mysql://%s:%d/%s", ip, port, database, encoding);
+        return DriverManager.getConnection(url, loginName, password);
+    }
+
+    public static void close(Statement stmt, Connection conn){
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(ResultSet res, Statement stmt, Connection conn){
+        if (res != null){
+            try {
+                res.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) throws SQLException {
         System.out.println(getConnection());
     }
+
 }
