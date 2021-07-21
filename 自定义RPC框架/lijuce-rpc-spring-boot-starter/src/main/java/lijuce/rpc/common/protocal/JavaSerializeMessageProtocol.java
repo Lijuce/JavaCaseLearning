886e06631ec0359240befa582a33f3571ec8493f
Lijuce <441214473@qq.com>
2021-07-22 00:26:10 +0800

    增强序列化机制

--- 自定义RPC框架/lijuce-rpc-spring-boot-starter/src/main/java/lijuce/rpc/common/protocal/JavaSerializeMessageProtocol.java
+++ 自定义RPC框架/lijuce-rpc-spring-boot-starter/src/main/java/lijuce/rpc/common/protocal/JavaSerializeMessageProtocol.java
@@ -1,5 +1,10 @@
 package lijuce.rpc.common.protocal;
 
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtobufIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
@@ -7,15 +12,25 @@ import java.io.ObjectOutputStream;
 
 /**
  * 序列化消息协议
+ * @author Lijuce
  */
 public class JavaSerializeMessageProtocol implements MessageProtocol{
+    static Schema<Object> schema = RuntimeSchema.getSchema(Object.class);
 
     private byte[] serialize(Object obj) throws Exception {
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(bout);
-        out.writeObject(obj);
-        return bout.toByteArray();
+        LinkedBuffer buffer = LinkedBuffer.allocate(512);
+        final byte[] protoStuff;
+        protoStuff = ProtobufIOUtil.toByteArray(obj, schema, buffer);
+        return protoStuff;
     }
+
+    private Object unSerialize(byte[] protoStuff){
+        // 读取对象，反序列化
+        Object obj = schema.newMessage();
+        ProtobufIOUtil.mergeFrom(protoStuff, obj, schema);
+        return obj;
+    }
+
     @Override
     public byte[] marshallingRequest(MyRequest req) throws Exception {
         return this.serialize(req);
@@ -23,8 +38,7 @@ public class JavaSerializeMessageProtocol implements MessageProtocol{
 
     @Override
     public MyRequest unmarshallingRequest(byte[] data) throws Exception {
-        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data));
-        return (MyRequest) in.readObject();
+        return (MyRequest) this.unSerialize(data);
     }
 
     @Override
@@ -34,7 +48,6 @@ public class JavaSerializeMessageProtocol implements MessageProtocol{
 
     @Override
     public MyResponse unmarshallingResponse(byte[] data) throws Exception {
-        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data));
-        return (MyResponse) in.readObject();
+        return (MyResponse) this.unSerialize(data);
     }
 }
