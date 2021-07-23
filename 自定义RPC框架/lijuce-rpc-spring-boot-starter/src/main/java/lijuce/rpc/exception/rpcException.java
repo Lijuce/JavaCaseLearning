94d4b18d620162e962e5dfccf2dc38fc032454ff
Lijuce <441214473@qq.com>
2021-07-23 10:17:26 +0800

    添加RPC自己的异常处理

---
+++ 自定义RPC框架/lijuce-rpc-spring-boot-starter/src/main/java/lijuce/rpc/exception/rpcException.java
@@ -0,0 +1,14 @@
+package lijuce.rpc.exception;
+
+/**
+ * @ClassName rpcException
+ * @Description TODO
+ * @Author Lijuce_K
+ * @Date 2021/7/23 10:14
+ * @Version 1.0
+ **/
+public class rpcException extends RuntimeException{
+    public rpcException(String message) {
+        super(message);
+    }
+}
